6bc6b3c3ff6a0dab7451015085da265ddd77e15f
stell-hawk <stell_hawk@mail.ru>
2021-04-10 09:28:49 +0700

    fix

--- yun-relay-32-32.ino
+++ yun-relay-32-32.ino
@@ -4,7 +4,6 @@
 #include <FileIO.h>
 #include <HttpClient.h>
 //#include <hashmap.h>
-#include "io.h"
 #define PORTSCOUNT 32
 //int inPorts[]={2,3,4,5,6,7,8,9,10,11,12,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34};
 //int outPorts[]={35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,53,A0,A1,A2,A3,A4,A5,A6,A7,A8,A9,A10,A11,A12,A13,A14,A15};
